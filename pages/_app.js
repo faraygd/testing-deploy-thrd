7bf9562cceab3ba227e2e644459a8c0e2e681b76
farayzk <aldifahriziilmawan@gmail.com>
2022-12-24 11:22:54 +0700

    testing deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import { ThirdwebProvider , ChainId} from '@thirdweb-dev/react'
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
