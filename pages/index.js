51cf907130c44d7589438e67bead7491cceabb3a
farayzk <aldifahriziilmawan@gmail.com>
2023-01-21 14:08:02 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,85 @@
-import {useAddress, useContract, useWalletConnect ,useMetamask, useCoinbaseWallet} from "@thirdweb-dev/react"
-
+import {
+  useAddress,
+  useContract,
+  useWalletConnect,
+  useMetamask,
+  useCoinbaseWallet,
+} from '@thirdweb-dev/react'
+import 'react-toastify/dist/ReactToastify.css'
+import { toast, ToastContainer, lo } from 'react-toastify'
 
 export default function Home() {
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const coinbaseWallet = useCoinbaseWallet();
-  const connectWallet = useWalletConnect();
-  const { contract : editionDrop, isLoading, isError} = useContract(
-    process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS,"edition-drop"
-  );
-  console.log({contract : editionDrop});
+  const address = useAddress()
+  const connectWithMetamask = useMetamask()
+  const coinbaseWallet = useCoinbaseWallet()
+  const connectWallet = useWalletConnect()
+  const { contract: editionDrop, isLoading, isError } = useContract(
+    process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS,
+    'edition-drop',
+  )
+  console.log(editionDrop)
 
-  const mintingNFT = async() => {
-    const tx = await editionDrop.erc1155.claimTo(address, 2 , 1 );
+  async function mintingNFT() {
+    if (!address) return
+    try {
+      const tx = await editionDrop.erc1155.claimTo(address, 2, 1)
+      toast.success("Success Minting")
+    } catch (err) {
+      console.log(err)
+      toast.error("Minting Error");
+    } finally {
+    }
   }
   // Condition
   // if(isLoading) {}
   //if(isError){}
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        reverseOrder={false}
+        theme="dark"
+      />
       <div className="flex text-center justify-center my-20">
-        { address ? <button className='font-bold border-line bg-red-300 w-40 h-10 ml-20'>{address?.substring(0,5)}...{address?.substring(address.length, address.length - 5)}</button> : <button className='font-bold border-line bg-red-300 w-40 h-10' onClick={connectWithMetamask} >Connect Wallet</button> }
-      <br></br>
-      { address ? <button className='font-bold border-line bg-red-300 w-40 h-10 ml-20'>{address?.substring(0,5)}...{address?.substring(address.length, address.length - 5)}</button> : <button className='font-bold border-line bg-red-300 w-40 h-10' onClick={coinbaseWallet} >Connect Wallet</button> }
-      <br></br>
-        <button className='font-bold border-line bg-red-300 w-40 h-10 ml-20'onClick={mintingNFT}>Minting NFT</button>
-      <br></br>
-      <button onClick={connectWallet}>Connect Wallet</button>
+        {address ? (
+          <button className="font-bold border-line bg-red-300 w-40 h-10 ml-20">
+            {address?.substring(0, 5)}...
+            {address?.substring(address.length, address.length - 5)}
+          </button>
+        ) : (
+          <button
+            className="font-bold border-line bg-red-300 w-40 h-10"
+            onClick={connectWithMetamask}
+          >
+            Connect Wallet
+          </button>
+        )}
+        <br></br>
+        {address ? (
+          <button className="font-bold border-line bg-red-300 w-40 h-10 ml-20">
+            {address?.substring(0, 5)}...
+            {address?.substring(address.length, address.length - 5)}
+          </button>
+        ) : (
+          <button
+            className="font-bold border-line bg-red-300 w-40 h-10"
+            onClick={coinbaseWallet}
+          >
+            Connect Wallet
+          </button>
+        )}
+        <br></br>
+        <button
+          className="font-bold border-line bg-red-300 w-40 h-10 ml-20"
+          onClick={mintingNFT}
+        >
+          Minting NFT
+        </button>
+        <br></br>
+        <button onClick={connectWallet}>Connect Wallet</button>
       </div>
-
     </>
   )
 }
